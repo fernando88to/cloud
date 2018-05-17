7082d01773c4384b9ea22404a7993ab149ab72c7
fernando88to <fernando88to@Gmail.com>
2018-05-17 17:26:41 -0300

    update

--- src/pages/resultado/resultado.ts
+++ src/pages/resultado/resultado.ts
@@ -15,7 +15,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ResultadoPage {
 
-  servico_mais_indicado: any;
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
@@ -23,12 +23,12 @@ export class ResultadoPage {
   ionViewDidLoad() {
       let servidores   =  {
           "servidores_cadastrados": [
-              {"id":1,"nome":"GetUpCloud", "moeda":"real", "pagamentofixo":true,"periodoteste":true,"servidornobrasil":true,"cotagratuitaproducao":false, "url":"https://getupcloud.com/pt-br/precos/", "quantidade_acertos":0},
-              {"id":2,"nome":"Google APP Engine", "moeda":"real", "pagamentofixo":false,"periodoteste":true,"servidornobrasil":true,"cotagratuitaproducao":true, "url":"https://cloud.google.com/appengine/", "quantidade_acertos":0},
-              {"id":3,"nome":"Google firebase", "moeda":"real", "pagamentofixo":true,"periodoteste":true,"servidornobrasil":false,"cotagratuitaproducao":false, "url":"https://firebase.google.com/pricing/?hl=pt-br", "quantidade_acertos":0},
-              {"id":4,"nome":"Heroku", "moeda":"dolar", "pagamentofixo":true,"periodoteste":true,"servidornobrasil":false,"cotagratuitaproducao":false, "url":"https://www.heroku.com/pricing", "quantidade_acertos":0},
-              {"id":5,"nome":"Windows Azure Cloud", "moeda":"real", "pagamentofixo":false,"periodoteste":true,"servidornobrasil":true,"cotagratuitaproducao":false, "url":"https://azure.microsoft.com/pt-br/pricing/calculator/", "quantidade_acertos":0},
-              {"id":6,"nome":"AWS Lambda", "moeda":"dolar", "pagamentofixo":false,"periodoteste":true,"servidornobrasil":true,"cotagratuitaproducao":false, "url":"https://aws.amazon.com/pt/lambda/pricing/", "quantidade_acertos":0}
+              {"id":1,"nome":"GetUpCloud", "moeda":"real", "pagamentofixo":true,"periodoteste":true,"servidornobrasil":true,"cotagratuitaproducao":false, "url":"https://getupcloud.com/pt-br/precos/", "pontuacao":0},
+              {"id":2,"nome":"Google APP Engine", "moeda":"real", "pagamentofixo":false,"periodoteste":true,"servidornobrasil":true,"cotagratuitaproducao":true, "url":"https://cloud.google.com/appengine/", "pontuacao":0},
+              {"id":3,"nome":"Google firebase", "moeda":"real", "pagamentofixo":true,"periodoteste":true,"servidornobrasil":false,"cotagratuitaproducao":false, "url":"https://firebase.google.com/pricing/?hl=pt-br", "pontuacao":0},
+              {"id":4,"nome":"Heroku", "moeda":"dolar", "pagamentofixo":true,"periodoteste":true,"servidornobrasil":false,"cotagratuitaproducao":false, "url":"https://www.heroku.com/pricing", "pontuacao":0},
+              {"id":5,"nome":"Windows Azure Cloud", "moeda":"real", "pagamentofixo":false,"periodoteste":true,"servidornobrasil":true,"cotagratuitaproducao":false, "url":"https://azure.microsoft.com/pt-br/pricing/calculator/", "pontuacao":0},
+              {"id":6,"nome":"AWS Lambda", "moeda":"dolar", "pagamentofixo":false,"periodoteste":true,"servidornobrasil":true,"cotagratuitaproducao":false, "url":"https://aws.amazon.com/pt/lambda/pricing/", "pontuacao":0}
           ]
       };
 
@@ -39,26 +39,24 @@ export class ResultadoPage {
 
       for (let value of servidores.servidores_cadastrados) {
           if(value.moeda==dados_formulario_usuario.moeda){
-            value.quantidade_acertos = value.quantidade_acertos +1 ;
+            value.pontuacao = value.pontuacao +1 ;
             //value.selecionado=false;
           }
           if(value.pagamentofixo==dados_formulario_usuario.pagamentofixo){
-              value.quantidade_acertos = value.quantidade_acertos +1 ;
+              value.pontuacao = value.pontuacao +1 ;
           }
 
           if(value.periodoteste==dados_formulario_usuario.periodoteste){
-              value.quantidade_acertos = value.quantidade_acertos +1 ;
+              value.pontuacao = value.pontuacao +1 ;
           }
 
           if(value.servidornobrasil==dados_formulario_usuario.servidornobrasil){
-              value.quantidade_acertos = value.quantidade_acertos +1 ;
-          }
-          if(value.cotagratuitaproducao==dados_formulario_usuario.cotagratuitaproducao){
-              value.quantidade_acertos = value.quantidade_acertos +1 ;
+              value.pontuacao = value.pontuacao +1 ;
           }
 
+
           if(value.cotagratuitaproducao==dados_formulario_usuario.cotagratuitaproducao){
-              value.quantidade_acertos = value.quantidade_acertos +1 ;
+              value.pontuacao = value.pontuacao +1 ;
           }
 
 
@@ -67,16 +65,14 @@ export class ResultadoPage {
 
 
       servidores.servidores_cadastrados.sort(function(a, b) {
-          return a.quantidade_acertos - b.quantidade_acertos;
+          return a.pontuacao - b.pontuacao;
       });
       servidores.servidores_cadastrados.reverse();
-      /*for (let value of servidores.servidores_cadastrados) {
-            console.log(value.nome + " " +  value.quantidade_acertos);
-      }*/
+      for (let value of servidores.servidores_cadastrados) {
+            console.log(value.nome + " " +  value.pontuacao);
+      }
 
-      this.servico_mais_indicado = servidores.servidores_cadastrados[0];
-      console.log(this.servico_mais_indicado);
-
+      //exibir a uma lista com o ranking pela pontuaçao, ao clicar no servidor mostrar os dados
 
 
 
--- www/build/main.js
+++ www/build/main.js
@@ -32,12 +32,12 @@ var ResultadoPage = /** @class */ (function () {
     ResultadoPage.prototype.ionViewDidLoad = function () {
         var servidores = {
             "servidores_cadastrados": [
-                { "id": 1, "nome": "GetUpCloud", "moeda": "real", "pagamentofixo": true, "periodoteste": true, "servidornobrasil": true, "cotagratuitaproducao": false, "url": "https://getupcloud.com/pt-br/precos/", "quantidade_acertos": 0 },
-                { "id": 2, "nome": "Google APP Engine", "moeda": "real", "pagamentofixo": false, "periodoteste": true, "servidornobrasil": true, "cotagratuitaproducao": true, "url": "https://cloud.google.com/appengine/", "quantidade_acertos": 0 },
-                { "id": 3, "nome": "Google firebase", "moeda": "real", "pagamentofixo": true, "periodoteste": true, "servidornobrasil": false, "cotagratuitaproducao": false, "url": "https://firebase.google.com/pricing/?hl=pt-br", "quantidade_acertos": 0 },
-                { "id": 4, "nome": "Heroku", "moeda": "dolar", "pagamentofixo": true, "periodoteste": true, "servidornobrasil": false, "cotagratuitaproducao": false, "url": "https://www.heroku.com/pricing", "quantidade_acertos": 0 },
-                { "id": 5, "nome": "Windows Azure Cloud", "moeda": "real", "pagamentofixo": false, "periodoteste": true, "servidornobrasil": true, "cotagratuitaproducao": false, "url": "https://azure.microsoft.com/pt-br/pricing/calculator/", "quantidade_acertos": 0 },
-                { "id": 6, "nome": "AWS Lambda", "moeda": "dolar", "pagamentofixo": false, "periodoteste": true, "servidornobrasil": true, "cotagratuitaproducao": false, "url": "https://aws.amazon.com/pt/lambda/pricing/", "quantidade_acertos": 0 }
+                { "id": 1, "nome": "GetUpCloud", "moeda": "real", "pagamentofixo": true, "periodoteste": true, "servidornobrasil": true, "cotagratuitaproducao": false, "url": "https://getupcloud.com/pt-br/precos/", "pontuacao": 0 },
+                { "id": 2, "nome": "Google APP Engine", "moeda": "real", "pagamentofixo": false, "periodoteste": true, "servidornobrasil": true, "cotagratuitaproducao": true, "url": "https://cloud.google.com/appengine/", "pontuacao": 0 },
+                { "id": 3, "nome": "Google firebase", "moeda": "real", "pagamentofixo": true, "periodoteste": true, "servidornobrasil": false, "cotagratuitaproducao": false, "url": "https://firebase.google.com/pricing/?hl=pt-br", "pontuacao": 0 },
+                { "id": 4, "nome": "Heroku", "moeda": "dolar", "pagamentofixo": true, "periodoteste": true, "servidornobrasil": false, "cotagratuitaproducao": false, "url": "https://www.heroku.com/pricing", "pontuacao": 0 },
+                { "id": 5, "nome": "Windows Azure Cloud", "moeda": "real", "pagamentofixo": false, "periodoteste": true, "servidornobrasil": true, "cotagratuitaproducao": false, "url": "https://azure.microsoft.com/pt-br/pricing/calculator/", "pontuacao": 0 },
+                { "id": 6, "nome": "AWS Lambda", "moeda": "dolar", "pagamentofixo": false, "periodoteste": true, "servidornobrasil": true, "cotagratuitaproducao": false, "url": "https://aws.amazon.com/pt/lambda/pricing/", "pontuacao": 0 }
             ]
         };
         //todos os parametros que o usuário informou
@@ -45,34 +45,31 @@ var ResultadoPage = /** @class */ (function () {
         for (var _i = 0, _a = servidores.servidores_cadastrados; _i < _a.length; _i++) {
             var value = _a[_i];
             if (value.moeda == dados_formulario_usuario.moeda) {
-                value.quantidade_acertos = value.quantidade_acertos + 1;
+                value.pontuacao = value.pontuacao + 1;
                 //value.selecionado=false;
             }
             if (value.pagamentofixo == dados_formulario_usuario.pagamentofixo) {
-                value.quantidade_acertos = value.quantidade_acertos + 1;
+                value.pontuacao = value.pontuacao + 1;
             }
             if (value.periodoteste == dados_formulario_usuario.periodoteste) {
-                value.quantidade_acertos = value.quantidade_acertos + 1;
+                value.pontuacao = value.pontuacao + 1;
             }
             if (value.servidornobrasil == dados_formulario_usuario.servidornobrasil) {
-                value.quantidade_acertos = value.quantidade_acertos + 1;
+                value.pontuacao = value.pontuacao + 1;
             }
             if (value.cotagratuitaproducao == dados_formulario_usuario.cotagratuitaproducao) {
-                value.quantidade_acertos = value.quantidade_acertos + 1;
-            }
-            if (value.cotagratuitaproducao == dados_formulario_usuario.cotagratuitaproducao) {
-                value.quantidade_acertos = value.quantidade_acertos + 1;
+                value.pontuacao = value.pontuacao + 1;
             }
         }
         servidores.servidores_cadastrados.sort(function (a, b) {
-            return a.quantidade_acertos - b.quantidade_acertos;
+            return a.pontuacao - b.pontuacao;
         });
         servidores.servidores_cadastrados.reverse();
-        /*for (let value of servidores.servidores_cadastrados) {
-              console.log(value.nome + " " +  value.quantidade_acertos);
-        }*/
-        this.servico_mais_indicado = servidores.servidores_cadastrados[0];
-        console.log(this.servico_mais_indicado);
+        for (var _b = 0, _c = servidores.servidores_cadastrados; _b < _c.length; _b++) {
+            var value = _c[_b];
+            console.log(value.nome + " " + value.pontuacao);
+        }
+        //exibir a uma lista com o ranking pela pontuaçao, ao clicar no servidor mostrar os dados
     };
     ResultadoPage = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
